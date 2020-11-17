a0a9c953349d0cfc7b18afc29b6ae3deb77a78e7
jimmy <jimmychan06@gmail.com>
2020-11-17 14:35:04 +0800

    updated soloEnv

--- environments/soloEnv.py
+++ environments/soloEnv.py
@@ -12,16 +12,22 @@ class SoloEnv(mujoco_env.MujocoEnv, utils.EzPickle):
                  terminate_when_unhealthy=True,
                  healthy_z_range=(0.17, 0.8),
                  healthy_y_range=(-0.8, 0.8),
-                 max_timestep=100
+                 max_timestep=100,
+                 max_distance=40
                 ):
 
         self._terminate_when_unhealthy = terminate_when_unhealthy
         self._healthy_z_range = healthy_z_range
         self._healthy_y_range = healthy_y_range
         self.max_timestep = max_timestep
+        self._max_distance = max_distance
         self.curr_timestep = 0
         mujoco_env.MujocoEnv.__init__(self, xml_file, 5)
         utils.EzPickle.__init__(self)
+
+    @property
+    def has_reached_max_distance(self):
+        return self.sim.data.qpos[0] > self._max_distance
 
     @property
     def get_body_Rot(self):
@@ -63,56 +69,67 @@ class SoloEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         x_vel = self.sim.data.qvel[0]
         yaw = abs(self.get_body_Rot[2])
         # print(yaw)
-        is_healthy = (min_z < self.sim.data.qpos[2] < max_z) and (min_y < self.sim.data.qpos[1] < max_y) and yaw < 35
+        is_healthy = (min_z < self.sim.data.qpos[2] < max_z) \
+            and (min_y < self.sim.data.qpos[1] < max_y) and yaw < 35
         return is_healthy
 
     @property
     def done(self):
-        done = (not self.is_healthy or self.curr_timestep > self.max_timestep
+        done = (not self.is_healthy or self.curr_timestep > self.max_timestep \
+                    or self.has_reached_max_distance
                 if self._terminate_when_unhealthy
                 else False)
         return done
 
     def step(self, action):
-        xposbefore = self.sim.data.qpos[0]
-        self.do_simulation(action, self.frame_skip)
-        self.curr_timestep += self.dt
-        xposafter = self.sim.data.qpos[0]
-        x_velocity = (xposafter - xposbefore) / self.dt
+        xposbefore = self.sim.data.qpos[0]              # Obtain position before action step
+        self.do_simulation(action, self.frame_skip)     # Take action step
+        self.curr_timestep += self.dt                   # Time step
+        xposafter = self.sim.data.qpos[0]               # Obtain position after action step
+        x_velocity = (xposafter - xposbefore) / self.dt # Calculate velocity along x (robot forward direction)
 
-        dist_bef = math.floor(xposbefore)
-        dist_after = math.floor(xposafter)
-        if dist_after > dist_bef:
-            dist_reward = 3*dist_after
-        else:
-            dist_reward = 0
-        # print(dist_reward)
+        dist_bef = math.floor(xposbefore)               # Round down position before action step
+        dist_after = math.floor(xposafter)              # Round down postion after action step
+        dist_reward = 0
+        if dist_after > dist_bef:                       # If distance is more than prev covered
+            dist_reward = 2*dist_after                  # Reward 3 pt * meter covered
         
-        # reward
-        forward_reward = 2 * x_velocity
-        rewards = forward_reward + dist_reward #+ pos_reward
+        goal_reward = 0
+        if (self.has_reached_max_distance):             # Reward if agent reached edge of the map @40m
+            goal_reward = 500
+        
+        forward_reward = 2 * x_velocity                      # Reward based on velocity
+        rewards = forward_reward + dist_reward + goal_reward # Summation of rewards
 
-        # costs
-        ctrl_cost = 0.1 * np.sum(np.square(action))
-        y_deviation_cost = 0.05 * np.square(self.sim.data.qpos[1])
+        ctrl_cost = 0.1 * np.sum(np.square(action))  # Cost of moving the joints, to minimise movement
+        y_deviation_cost = 0.1 * np.square(self.sim.data.qpos[1]) # Penalty for deviation away from x-axis
+        x_absRot_cost = self.x_absRot_cost           # Penalty for rotation about x-axis
+        y_absRot_cost = self.y_absRot_cost           # Penalty for rotation about y-axis
+        z_absRot_cost = self.z_absRot_cost           # Penalty for rotation about z-axis
 
-        costs = ctrl_cost + self.x_absRot_cost + self.y_absRot_cost + self.z_absRot_cost + y_deviation_cost
-
-        # summation of all rewards
+        # Summation of all costs
+        costs = ctrl_cost + y_deviation_cost + x_absRot_cost + y_absRot_cost + z_absRot_cost
+        
+        # Summation of all rewards and costs
         reward = rewards - costs
 
         done = self.done
         observation = self._get_obs()
         info = {
-            'forward_reward': forward_reward,
-            'ctrl_cost': ctrl_cost,
             'dist_reward': dist_reward,
+            'forward_reward': forward_reward,
+            'goal_reward': goal_reward,
+
+            'ctrl_cost': ctrl_cost,
+            'x_absRot_cost': x_absRot_cost,
+            'y_absRot_cost': y_absRot_cost,
+            'z_absRot_cost': z_absRot_cost,
+            'y_deviation_cost': y_deviation_cost,
 
             'rewards': rewards,
             'costs': costs,
             'dist_from_origin': xposafter
         }
-        # print(info)
         return observation, reward, done, info
 
     def _get_obs(self):
